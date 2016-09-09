7b45514d824f09bbcf94ffef99c2852f5d7d0aac
Charles Xu <admin@Kthisiscvpv.com>
2016-09-09 08:52:24 -0400

    Exercise 1 - Review R4

--- src/review/Review2.java
+++ src/review/Review2.java
@@ -1,6 +1,10 @@
 package review;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
 import java.util.Scanner;
 
 public class Review2 {
@@ -12,14 +16,17 @@ public class Review2 {
 		String sentence = scanner.nextLine();
 
 		String[] words = sentence.split(" ");
+		List<String> wordsList = new ArrayList<String>(Arrays.asList(words));
 
-		for (int i = 0; i < words.length; i++)
-			words[i] = words[i].toLowerCase();
+		Collections.sort(wordsList, new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return o1.toLowerCase().compareTo(o2.toLowerCase());
+			}
+		});
 
-		Arrays.sort(words);
-
-		for (int i = 0; i < words.length; i++)
-			System.out.println(words[i]);
+		for (String word : wordsList)
+			System.out.println(word);
 
 		scanner.close();
 	}
