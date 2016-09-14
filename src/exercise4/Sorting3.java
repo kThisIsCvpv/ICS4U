0a06ef5c5a45b4bb871edb7690634ce0d189ab64
Charles Xu <admin@Kthisiscvpv.com>
2016-09-14 09:00:44 -0400

    Sorting Assignments R2

--- src/exercise4/Sorting3.java
+++ src/exercise4/Sorting3.java
@@ -10,15 +10,23 @@ package exercise4;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.LinkedHashMap;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.Map.Entry;
 
 public class Sorting3 {
+
+	public static class Entry {
+
+		public char character;
+		public int count;
+
+		public Entry(char character, int count) {
+			this.character = character;
+			this.count = count;
+		}
+	}
 
 	public static void main(String[] args) throws Exception {
 
@@ -41,26 +49,22 @@ public class Sorting3 {
 			}
 		}
 
-		Map<Character, Integer> count = new LinkedHashMap<Character, Integer>();
-
+		List<Entry> count = new ArrayList<Entry>();
 		for (int i = 0; i < appearance.length; i++) {
 			char reference = (char) (i + 65);
-			count.put(reference, appearance[i]);
+			count.add(new Entry(reference, appearance[i]));
 		}
 
-		List<Entry<Character, Integer>> entries = new LinkedList<Entry<Character, Integer>>();
-		entries.addAll(count.entrySet());
-
-		Collections.sort(entries, new Comparator<Entry<Character, Integer>>() {
-			public int compare(Entry<Character, Integer> o1, Entry<Character, Integer> o2) {
-				return o1.getValue().compareTo(o2.getValue()) * -1;
+		Collections.sort(count, new Comparator<Entry>() {
+			public int compare(Entry o1, Entry o2) {
+				return Integer.compare(o1.count, o2.count) * -1;
 			}
 		});
 
-		for (Entry<Character, Integer> entry : entries) {
-			double prob = ((double) entry.getValue()) / totalCharacters * 1000d;
+		for (Entry entry : count) {
+			double prob = ((double) entry.count) / totalCharacters * 1000d;
 			prob = Math.round(prob) / 10d;
-			System.out.println(entry.getKey() + " appears " + entry.getValue() + " different times (" + prob + "%).");
+			System.out.println(entry.character + " appears " + entry.count + " different times (" + prob + "%).");
 		}
 
 		br.close();
